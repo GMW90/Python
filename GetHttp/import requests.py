6cca7673b6cadf42bfda22f8e2571fb8837a981b
GMW <walid.gargouri@gmail.com>
2018-12-17 17:48:49 +0100

    change api key

--- GetHttp/import requests.py
+++ GetHttp/import requests.py
@@ -1,6 +1,6 @@
 import requests
-url="https://oc-jswebsrv.herokuapp.com/api/articles"
+url="http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=a78ee777a5e02651271cf5a820129f2b"
 content=requests.get(url)
 data=content.json()
-# t=data['main']['temp']
+t=data['main']['temp']
 print("La témpérature est de {} degrés C".format(t-273.15))
