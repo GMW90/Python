d4ca3c0ddf58181cb7679360d46e44a2b1db3a89
GMW <walid.gargouri@gmail.com>
2018-12-18 18:30:02 +0100

    fichier doublon

--- GetHttp/import requests.py
+++
@@ -1,6 +0,0 @@
-import requests
-url="http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=a78ee777a5e02651271cf5a820129f2b"
-content=requests.get(url)
-data=content.json()
-t=data['main']['temp']
-print("La témpérature est de {} degrés C".format(t-273.15))
